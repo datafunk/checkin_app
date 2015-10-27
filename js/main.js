9f918cfa8660d4fa92319facecbae60075e7d3b5
Imad Sader <imadsader@gmail.com>
2015-10-27 15:33:35 +1100

    commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 (function () {
 	
-	var teams = [],
-		users = [],
-		checkins = [];
+	var teams = [
+			{"teamId": "1233", "teamName": "Team1"},
+			{"teamId": "1233", "teamName": "Team2"}
+		],
+		users = [
+			{"userId": "987", "userName": "Imad"},
+			{"userId": "987", "userName": "Imad"},
+			{"userId": "987", "userName": "Imad"}
+		],
+		checkins = [
+			{"userId": "987", "date": "", "yesterday": "", "today": "", "blockers": ""},
+			{"userId": "987", "date": "", "yesterday": "", "today": "", "blockers": ""},
+			{"userId": "987", "date": "", "yesterday": "", "today": "", "blockers": ""},
+			{"userId": "987", "date": "", "yesterday": "", "today": "", "blockers": ""}
+		];
 
 	function readData () {
-		
+
 	}
 
 	function writeData () {
@@ -15,5 +27,21 @@
 			checkins: checkins
 		});
 	}
+
+	function populateTeams () {
+		_.each(teams, function(team) {
+			console.log(team);
+		});
+	}
+
+	function populateUsers (teamName) {
+		_.each(users, function (user) {
+			console.log(user);
+		});
+	}
+
+
+	populateTeams();
+	populateUsers();
 
 })();
