1db90e16b33edfdf999d750a641f178d879b4fd0
datafunk <pborbely@fairfaxmedia.com.au>
2015-10-27 16:57:43 +1100

    data

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
 (function () {
 	"use strict";
-	
+
 	var teams = [
 			{"teamName": "Team1"},
 			{"teamName": "Team2"}
 		],
 		users = [
 			{"userId": "987", "userName": "Imad", "teamName": "Team1"},
-			{"userId": "986", "userName": "Imad", "teamName": "Team1"},
-			{"userId": "985", "userName": "Imad", "teamName": "Team2"}
+			{"userId": "986", "userName": "Pete", "teamName": "Team1"},
+			{"userId": "985", "userName": "Imad2", "teamName": "Team2"},
+			{"userId": "984", "userName": "Pete2", "teamName": "Team2"},
+			{"userId": "983", "userName": "Imad3", "teamName": "Team3"},
+			{"userId": "982", "userName": "pete3", "teamName": "Team3"}
 		],
 		checkins = [
-			{"userId": "987", "date": "2015-10-26", "yesterday": "", "today": "", "blockers": ""},
-			{"userId": "986", "date": "2015-10-26", "yesterday": "", "today": "", "blockers": ""},
-			{"userId": "985", "date": "2015-10-26", "yesterday": "", "today": "", "blockers": ""},
-			{"userId": "986", "date": "2015-10-27", "yesterday": "", "today": "", "blockers": ""}
+			{"userId": "987", "date": "2015-10-26", "yesterday": "Ate pie", "today": "will do the same", "blockers": "exist, but hey"},
+			{"userId": "986", "date": "2015-10-26", "yesterday": "Ate pizza", "today": "will do different", "blockers": "exist, but hey"},
+			{"userId": "985", "date": "2015-10-26", "yesterday": "Ate salad", "today": "will do something", "blockers": "exist, but hey"},
+			{"userId": "987", "date": "2015-10-27", "yesterday": "Ate hot dogs", "today": "will do whatever", "blockers": "exist, but hey"},
+			{"userId": "986", "date": "2015-10-27", "yesterday": "Ate cookies", "today": "will do, promise", "blockers": "exist, but hey"},
+			{"userId": "985", "date": "2015-10-27", "yesterday": "Ate cake", "today": "will do if have to", "blockers": "exist, but hey"},
+			{"userId": "987", "date": "2015-10-28", "yesterday": "Drunk coke", "today": "will do, NOT!", "blockers": "exist, but hey"},
+			{"userId": "986", "date": "2015-10-28", "yesterday": "Drunk beer", "today": "will do, do do do do do", "blockers": "exist, but hey"},
+			{"userId": "985", "date": "2015-10-28", "yesterday": "Drunk soda", "today": "ha!?", "blockers": "exist, but hey"},
+			{"userId": "987", "date": "2015-10-29", "yesterday": "Drunk limonade", "today": "will try", "blockers": "exist, but hey"},
+			{"userId": "986", "date": "2015-10-29", "yesterday": "Drunk water", "today": "will do or will not", "blockers": "exist, but hey"},
+			{"userId": "985", "date": "2015-10-29", "yesterday": "Drunk coffee", "today": "will do the same again", "blockers": "exist, but hey"}
 		],
 		userInputs = {};
 
@@ -92,7 +103,7 @@ console.log(teamName, usersArray);
 
 
 	setMaxDate();
-	
+
 	$("#teams").append(populateTeams());
 
 	$('#teams').on('change', function(e){
@@ -102,7 +113,7 @@ console.log(teamName, usersArray);
     });
 
 	$('#date').on('change', function(e){
-        
+
         populateList();
     });
 
